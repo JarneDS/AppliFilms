4b66d3dd633e6d3397645ee9a7766361bf38bbe7
Jarne <descheerderjarne@gmail.com>
2025-04-02 17:18:01 +0200

    Début

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,11 @@
-console.info('Hello world');
+"use strict";
+
+const apiURL = `https://web.mayfly.ovh/proxy/movie.php?endpoint=search/movie?query=avengers`;
+
+  fetch(apiURL)
+      .then(response => {
+          if (!response.ok) {
+              throw new Error('Film pas trouver');
+          }
+          return response.json();
+      })
